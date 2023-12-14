5b1b9f02fc3887d3e2f9e2906b35c71e3f12e95c
CJBreen <B00152224@mytudublin.ie>
2023-12-14 16:58:23 +0000

    made main menu + pause menu

--- Assets/Scripts/GUI.cs
+++ Assets/Scripts/GUI.cs
@@ -9,7 +9,6 @@ public class GUI : MonoBehaviour
 {
     private TextMeshProUGUI playerSpeed;
     private Rigidbody playerRb;
-    private Canvas escMenu;
     private Canvas hudMenu;
 
     public bool isEsc;
@@ -18,10 +17,8 @@ public class GUI : MonoBehaviour
     void Start()
     {
         playerSpeed = GameObject.Find("PlayerSpeed").GetComponent<TextMeshProUGUI>();
-        escMenu = GameObject.Find("EscMenu").GetComponent<Canvas>();
         hudMenu = GameObject.Find("HUD").GetComponent<Canvas>();
         playerRb = GameObject.Find("PlayerObject").GetComponent<Rigidbody>();
-        escMenu.enabled = false;
     }
 
     void Update()
@@ -29,27 +26,5 @@ public class GUI : MonoBehaviour
         playerSpeed.text = "Speed: "+Mathf.RoundToInt(Mathf.Abs(playerRb.velocity.magnitude)) + " m/s";
         // playerSpeed.text = "Speed: "+Mathf.RoundToInt(Mathf.Abs(playerRb.velocity.magnitude)*0.2f) + " b/s";    // Bananas Per Second
     }
-    void FixedUpdate() {
-        if (Input.GetKey(KeyCode.Escape) && allowGuiActivate) {
-            if (!isEsc){
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
-                hudMenu.enabled = false;
-                escMenu.enabled = true;
-                isEsc = true;
-            }
-            else {
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
-                hudMenu.enabled = true;
-                escMenu.enabled = false;
-                isEsc = false;
-            }
-            allowGuiActivate = false;
-            Invoke("guiActivateCooldown", 1);
-        }
-    }
-    private void guiActivateCooldown() {
-        allowGuiActivate = true;
-    }
+  
 }
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+
+    public GameObject pauseUI;
+    public GameObject HUD;
+    
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (isPaused)
+            {
+                resumeGame();
+            }
+            else
+            {
+                pauseGame();
+            }
+        }
+    }
+
+    public void resumeGame()
+    {
+        pauseUI.SetActive(false);
+        HUD.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void pauseGame()
+    {
+        pauseUI.SetActive(true);
+        HUD.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+        isPaused = true;
+
+    }
+
+
+public void openGit()
+    {
+        Application.OpenURL("https://github.com/CJBreen/GrapplerFinal");
+    }
+
+    public void returntoMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
