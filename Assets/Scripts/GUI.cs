fcf4719540162f168572301e91fae5276f4167a8
CJBreen <B00152224@mytudublin.ie>
2023-12-16 23:33:45 +0000

    broke the text
    sorry lukas

--- Assets/Scripts/GUI.cs
+++ Assets/Scripts/GUI.cs
@@ -10,6 +10,8 @@ public class GUI : MonoBehaviour
     private TextMeshProUGUI playerSpeed;
     private Rigidbody playerRb;
     private Canvas hudMenu;
+    private TextMeshProUGUI enemiesRemaining;
+    private GameObject[] numOfEnemies;
 
     public bool isEsc;
 
@@ -17,12 +19,15 @@ public class GUI : MonoBehaviour
     {
         playerSpeed = GameObject.Find("PlayerSpeed").GetComponent<TextMeshProUGUI>();
         hudMenu = GameObject.Find("HUD").GetComponent<Canvas>();
+        enemiesRemaining = GameObject.Find("Enemies").GetComponent<TextMeshProUGUI>();
         playerRb = GameObject.Find("PlayerObject").GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        numOfEnemies = GameObject.FindGameObjectsWithTag("Enemy");
         playerSpeed.text = "Speed: "+Mathf.RoundToInt(Mathf.Abs(playerRb.velocity.magnitude)) + " m/s";
+        enemiesRemaining.text = "Enemies Remaining:" + numOfEnemies.Length;
         // playerSpeed.text = "Speed: "+Mathf.RoundToInt(Mathf.Abs(playerRb.velocity.magnitude)*0.2f) + " b/s";    // Bananas Per Second
     }
   
